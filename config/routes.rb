00d43fcf1d710f9f27949d9375fd85bca728e0c0
Alizée WYART <alizee.wyart@gmail.com>
2016-11-21 17:58:05 +0100

    update routes.rb to add routes for contributions

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :projects, only: [:index, :show, :new, :create, :edit, :update]
+  resources :projects, only: [:index, :show, :new, :create, :edit, :update] do 
+    resources :contributions, only: [:new, :create]
+  end
 end
