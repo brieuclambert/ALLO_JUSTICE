750f45e883fd7d1d13cfc6b3021ba96178e86b9e
Alizée WYART <alizee.wyart@gmail.com>
2016-11-21 18:21:53 +0100

    Update contribution.rb

--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -2,5 +2,4 @@ class Contribution < ApplicationRecord
   belongs_to :users
   belongs_to :projects
   validates :amount, presence: true
-  validates :amount, numericality: { only_integer: true }
 end
