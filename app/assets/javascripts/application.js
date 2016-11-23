2f8ad45e6b1e9eff5c0a6455d1f86d1051c11978
Brieuc Lambert <lambert.brieuc@gmail.com>
2016-11-23 15:55:09 +0100

    test

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,10 +1,11 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
+//= require underscore
+//= require gmaps/google
 //= require_tree .
 //= require jquery-fileupload/basic
 //= require cloudinary/jquery.cloudinary
 //= require attachinary
 //= require bootstrap-datepicker
-//= require underscore
-//= require gmaps/google
+
