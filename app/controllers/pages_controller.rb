393214d95a664322fa2693c196fb060894eb7a5d
Alizee Wyart <alizee.wyart@gmail.com>
2016-11-23 12:11:05 +0100

    add map static

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,4 +9,18 @@ class PagesController < ApplicationController
     @contributions = Contribution.select{ |contribution| contribution.user_id == current_user.id}
     @projects = Project.select{ |project| project.user_id == current_user.id}
   end
+
+  def map
+    @markers = []
+    Project.all.each do |project|
+      if project.latitude && project.longitude
+        @markers << {
+          "lat": project.latitude,
+          "lng": project.longitude,
+          "infowindow": "<h1>#{project.name}</h1>"
+        }
+      end
+    end
+    # raise
+  end
 end
