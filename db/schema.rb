6966e9d716f5bfb3304d214af5f24d40402859c5
Alizee Wyart <alizee.wyart@gmail.com>
2016-11-23 11:09:32 +0100

    add coordinates to projects db / model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161123095222) do
+ActiveRecord::Schema.define(version: 20161123100835) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,6 +61,8 @@ ActiveRecord::Schema.define(version: 20161123095222) do
     t.string   "category"
     t.date     "echeance"
     t.string   "address"
+    t.float    "latitude"
+    t.float    "longitude"
     t.index ["user_id"], name: "index_projects_on_user_id", using: :btree
   end
 
